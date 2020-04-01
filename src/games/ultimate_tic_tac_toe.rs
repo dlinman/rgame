9b45ec48d9155c0927f6b59fbb832713d03b9ff6
David L. Inman <dlinman@sep.com>
2020-04-01 13:44:00 -0400

    ultimate tic tac toe game implementation

--- src/games/ultimate_tic_tac_toe.rs
+++ src/games/ultimate_tic_tac_toe.rs
@@ -7,7 +7,7 @@ pub fn get_game() -> impl Game {
 
 struct Ultimate { }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 enum Square {
     X,
     O,
@@ -19,6 +19,7 @@ enum MiniBoard {
     X,
     O,
     Board(Vec<Vec<Square>>),
+    Draw(Vec<Vec<Square>>),
 }
 
 #[derive(Clone)]
@@ -35,18 +36,72 @@ struct UState {
 }
 
 struct UTurn {
-
+    row : usize,
+    col : usize,
+    mini_board_row : usize,
+    mini_board_col : usize,
+    player_turn : u32,
+    square : Square,
 }
 
 struct Turns {
-
+    s0 : UState,
+    row : usize,
+    col : usize,
+    mini_row : usize,
+    mini_col : usize,
 }
 
 impl Iterator for Turns {
     type Item = UTurn;
 
     fn next(&mut self) -> Option<UTurn> {
-        None
+        loop {
+            if self.mini_row == 3 {
+                self.mini_row = 0;
+                self.mini_col = self.mini_col + 1;
+            }
+            if self.mini_col == 3 {
+                self.mini_col = 0;
+                self.row = self.row + 1;
+            }
+            if self.row == 3 {
+                self.row = 0;
+                self.col = self.col + 1;
+            }
+            if self.col == 3 {
+                return None;
+            }
+            
+            if matches!(self.s0.board[self.row][self.col], MiniBoard::Board(_)) {
+                match &self.s0.board[self.row][self.col] {
+                    MiniBoard::Board(b) => {
+                        if matches!(b[self.mini_row][self.mini_col], Square::Empty) {
+                            break;
+                        }
+                    }
+                    _ => panic!("Mini board should be board"),
+                }
+            }
+            self.mini_row = self.mini_row + 1;
+        }
+
+        let square = if self.s0.player_turn == 0 {
+            Square::X
+        }
+        else {
+            Square::O
+        };
+
+        let ret = UTurn{ row: self.row
+                       , col: self.col
+                       , mini_board_row: self.mini_row
+                       , mini_board_col: self.mini_col
+                       , square
+                       , player_turn: self.s0.player_turn
+                       };
+        self.row = self.row + 1;
+        Some(ret)
     }
 }
 
@@ -85,11 +140,36 @@ impl Game for Ultimate {
     }
 
     fn take_turn(&self, state : &UState, turn_action : &UTurn) -> UState {
-        state.clone()
+        let (r, c) = match state.target_board {
+            BoardAllowed::Any => (turn_action.row, turn_action.col),
+            BoardAllowed::At{row,col} => (row,col),
+        };
+
+        let mut new_state = state.clone();
+        let mini_board = match &mut new_state.board[r][c] {
+            MiniBoard::Board(b) => b,
+            _ => panic!("Attempting to make move on completed board")
+        };
+        mini_board[turn_action.mini_board_row][turn_action.mini_board_col] = turn_action.square;
+        match check_mini_status(mini_board) {
+            GameResult::Winner {player, scores: _} if player == 0 => new_state.board[r][c] = MiniBoard::X,
+            GameResult::Winner {player, scores: _} => new_state.board[r][c] = MiniBoard::O,
+            GameResult::Draw {scores: _} => new_state.board[r][c] = MiniBoard::Draw(vec![]), // TODO 
+            GameResult::NotFinished => (),
+        }
+
+        new_state.player_turn = (state.player_turn + 1) % 2;
+
+        new_state.target_board = match new_state.board[turn_action.mini_board_row][turn_action.mini_board_col] {
+            MiniBoard::Board(_) => BoardAllowed::At { row: turn_action.mini_board_row, col: turn_action.mini_board_col },
+            _ => BoardAllowed::Any,
+        };
+
+        new_state
     }
 
     fn legal_turns(&self, state : &UState) -> Turns {
-        Turns {}
+        Turns { s0: state.clone(), row: 0, col: 0, mini_row: 0, mini_col: 0 }
     }
 
     fn state_score(&self, state : &UState, player : u32) -> i32 {
@@ -103,15 +183,183 @@ impl Game for Ultimate {
     }
 
     fn display_turn(&self, turn : &UTurn) -> String {
-        "".to_string()
+        fn p(s : Square) -> char {
+            match s {
+                Square::X => 'X',
+                Square::O => 'O',
+                Square::Empty => ' ',
+            }
+        }
+        format!( "Player {}: Board Row = {}; Board Col = {}; MiniBoard Row = {}; MiniBoard Col = {}; Symbol = {}\n", 
+            turn.player_turn + 1, 
+            turn.row, 
+            turn.col, 
+            turn.mini_board_row,
+            turn.mini_board_col,
+            p(turn.square) )
     }
 
     fn display_state(&self, state : &UState) -> String {
-        "".to_string()
+        fn sq(s : Square) -> char {
+            match s {
+                Square::X => 'X',
+                Square::O => 'O',
+                Square::Empty => ' ',
+            }
+        }
+        fn mini(b : &Vec<Vec<Square>>) -> [String; 3] {
+            let mut ret = ["".to_string(), "".to_string(), "".to_string()];
+            for r in 0..3 {
+                ret[r] = format!(" {} | {} | {} ", sq(b[r][0]), sq(b[r][1]), sq(b[r][2]));
+            }
+            ret
+        }
+        fn display(b : &MiniBoard) -> [String; 3] {
+            let big_x = ["\\ /".to_string(), " . ".to_string(), "/ \\".to_string()];
+            let big_o = ["___".to_string(), "| |".to_string(), "---".to_string()];
+            match b {
+                MiniBoard::X => big_x,
+                MiniBoard::O => big_o,
+                MiniBoard::Board(board) => mini(board),
+                MiniBoard::Draw(board) => mini(board),
+            }
+        }
+
+        let mut dis = vec![];
+        dis.push("\n".to_string());
+
+        for r in 0..3 {
+            let m0 = display(&state.board[r][0]);
+            let m1 = display(&state.board[r][1]);
+            let m2 = display(&state.board[r][2]);
+            dis.push(format!("{}||{}||{}", m0[0], m1[0], m2[0]));
+            dis.push(format!("{}||{}||{}", m0[1], m1[1], m2[1]));
+            dis.push(format!("{}||{}||{}", m0[2], m1[2], m2[2]));
+        }
+
+        dis.push("\n".to_string());
+        dis.into_iter().collect::<String>()
     }
 
     fn game_status(&self, state : &UState) -> GameResult {
-        GameResult::NotFinished
+        use MiniBoard::*;
+        use crate::games::game_form::GameResult::*;
+
+        fn row_won(line : &[MiniBoard]) -> Option<GameResult> {
+            match line {
+                [X, X, X] => Some( Winner { player: 0, scores: vec![] } ),
+                [O, O, O] => Some( Winner { player: 1, scores: vec![] } ),
+                _ => None,
+            }
+        }
+
+        fn won(line : &[&MiniBoard]) -> Option<GameResult> {
+            match line {
+                [X, X, X] => Some( Winner { player: 0, scores: vec![] } ),
+                [O, O, O] => Some( Winner { player: 1, scores: vec![] } ),
+                _ => None,
+            }
+        }
+
+        for r in 0..3 {
+            let result = row_won(&state.board[r][..]); 
+            if matches!(result, Some(_)) {
+                return result.unwrap();
+            }
+        }
+
+        let c0 = vec![ &state.board[0][0], &state.board[1][0], &state.board[2][0] ];
+        let result_c0 = won(&c0[..]); 
+        if matches!(result_c0, Some(_)) {
+            return result_c0.unwrap();
+        }
+
+        let c1 = vec![ &state.board[0][1], &state.board[1][1], &state.board[2][1] ];
+        let result_c1 = won(&c1); 
+        if matches!(result_c1, Some(_)) {
+            return result_c1.unwrap();
+        }
+
+        let c2 = vec![ &state.board[0][2], &state.board[1][2], &state.board[2][2] ];
+        let result_c2 = won(&c2); 
+        if matches!(result_c2, Some(_)) {
+            return result_c2.unwrap();
+        }
+
+        let d0 = vec![ &state.board[0][0], &state.board[1][1], &state.board[2][2] ];
+        let result_d0 = won(&d0); 
+        if matches!(result_d0, Some(_)) {
+            return result_d0.unwrap();
+        }
+
+        let d1 = vec![ &state.board[2][0], &state.board[1][1], &state.board[0][2] ];
+        let result_d1 = won(&d1); 
+        if matches!(result_d1, Some(_)) {
+            return result_d1.unwrap();
+        }
+
+        if state.board.iter().map(|row| row.iter()).flatten().filter(|s| matches!(s, MiniBoard::Board(_))).count() == 0 {
+            GameResult::Draw { scores: vec![] }
+        } 
+        else {
+            GameResult::NotFinished
+        }
     }
 }
 
+
+fn check_mini_status(mini : &Vec<Vec<Square>>) -> GameResult {
+    use Square::*;
+    use crate::games::game_form::GameResult::*;
+    fn won(line : &[Square]) -> Option<GameResult> {
+        match line {
+            [X, X, X] => Some( Winner { player: 0, scores: vec![] } ),
+            [O, O, O] => Some( Winner { player: 1, scores: vec![] } ),
+            _ => None,
+        }
+    }
+
+    for r in 0..3 {
+        let result = won(&mini[r][..]); 
+        if matches!(result, Some(_)) {
+            return result.unwrap();
+        }
+    }
+
+    let c0 = vec![ mini[0][0], mini[1][0], mini[2][0] ];
+    let result_c0 = won(&c0); 
+    if matches!(result_c0, Some(_)) {
+        return result_c0.unwrap();
+    }
+
+    let c1 = vec![ mini[0][1], mini[1][1], mini[2][1] ];
+    let result_c1 = won(&c1); 
+    if matches!(result_c1, Some(_)) {
+        return result_c1.unwrap();
+    }
+
+    let c2 = vec![ mini[0][2], mini[1][2], mini[2][2] ];
+    let result_c2 = won(&c2); 
+    if matches!(result_c2, Some(_)) {
+        return result_c2.unwrap();
+    }
+
+    let d0 = vec![ mini[0][0], mini[1][1], mini[2][2] ];
+    let result_d0 = won(&d0); 
+    if matches!(result_d0, Some(_)) {
+        return result_d0.unwrap();
+    }
+
+    let d1 = vec![ mini[2][0], mini[1][1], mini[0][2] ];
+    let result_d1 = won(&d1); 
+    if matches!(result_d1, Some(_)) {
+        return result_d1.unwrap();
+    }
+
+    if mini.iter().map(|row| row.iter()).flatten().filter(|s| matches!(s, Square::Empty)).count() == 0 {
+        GameResult::Draw { scores: vec![] }
+    } 
+    else {
+        GameResult::NotFinished
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ use agents::{strict_random_agent, pick_highest_action_agent, self_minimax_agent,
 
 
 fn main() {
-    let ttt = games::tic_tac_toe::get_game();
-    let mut p1 = pick_highest_action_agent::get_agent(0);
-    let mut p2 = human_player::get_agent();
+    let ttt = games::ultimate_tic_tac_toe::get_game();
+    let mut p1 = strict_random_agent::get_agent();
+    let mut p2 = strict_random_agent::get_agent();
     single_game::play(&ttt, &mut p1, &mut p2);
     /*let mut p1 = self_minimax_agent::get_agent(0, 3); 
     let mut p2 = pick_highest_action_agent::get_agent(1);
