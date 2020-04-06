2b1065e1468006d8d20450a29dd9ade6567ad601
David L. Inman <dlinman@sep.com>
2020-04-06 14:34:24 -0400

    main setup to check ultimate

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use agents::{strict_random_agent, pick_highest_action_agent, self_minimax_agent,
 fn main() {
     let ttt = games::ultimate_tic_tac_toe::get_game();
     let mut p1 = strict_random_agent::get_agent();
-    let mut p2 = strict_random_agent::get_agent();
+    let mut p2 = pick_highest_action_agent::get_agent(HeuristicDescription::Default, 1);
     single_game::play(&ttt, &mut p1, &mut p2);
     /*let ttt = games::tic_tac_toe::get_game();
     let mut p1 = self_minimax_agent::get_agent(HeuristicDescription::Default, 0, 3); 
